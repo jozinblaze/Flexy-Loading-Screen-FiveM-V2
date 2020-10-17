c5afd1fe40a5c309754f396cadbc0cd8f6af59c1
Redven-Biker <marween35620@gmail.com>
2020-10-18 00:04:10 +0200

    1.0

---
+++ README.md
@@ -0,0 +1,9 @@
+Discord : https://discord.gg/QW4TDJP
+
+![fivem](https://i.imgur.com/0BBTdwA.png)
+
+Vidéo : https://youtu.be/2U83PEMqtVc
+
+
+Gros bigup à Atndesign pour m'avoir aider
+<a class="github-button" href="https://github.com/Atndesign" data-size="large" aria-label="Follow @Atndesign on GitHub">Follow @Atndesign</a>
---
+++ __resource.lua
@@ -0,0 +1,25 @@
+files {
+    'index.html',
+    'css/style.css',
+    'css/font.css',
+    'img/logo.png',
+    'img/background.jpg',
+    'font/Orbitron-Black',
+    'font/Orbitron-Black.woff2',
+    'font/Orbitron-Bold',
+    'font/Orbitron-Bold.woff2',
+    'font/Orbitron-ExtraBold',
+    'font/Orbitron-ExtraBold.woff2',
+    'font/Orbitron-Medium',
+    'font/Orbitron-Medium.woff2',
+    'font/Orbitron-Regular',
+    'font/Orbitron-Regular.woff2',
+    'font/Orbitron-SemiBold',
+    'font/Orbitron-SemiBold.woff2'
+}
+
+loadscreen 'index.html'
+loadscreen_manual_shutdown 'yes'
+client_script 'client.lua'
+
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
